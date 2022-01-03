9d45fd50ef1c21e9644f71c70915ff98d629ac93
swmahardhika <swmahardhika@gmail.com>
2022-01-03 22:32:15 +0700

    Add project files.

---
+++ ServerConfigRest_20190140131_Satria Wirabuana M/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using ServiceRest_20190140131_Satria_Wirabuana_M;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_20190140131_Satria_Wirabuana_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready .. ");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
